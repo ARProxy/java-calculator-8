506f70f6f037c1c7c48615d1141d6d848772bd32
ARProxy <hocci0222@kakao.com>
2025-10-19 04:05:58 +0900

    👔 Feat: 예외가 발생하면 애플리케이션을 종료한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,7 +6,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         var service = new CalculatorService();
-        String result = service.add(Console.readLine());
-        System.out.println(result);
+        while(true) {
+            try {
+                String result = service.add(Console.readLine());
+                System.out.println(result);
+            } catch (IllegalArgumentException e) {
+                break;
+            }
+        }
     }
 }
